df0b40cfbc3838010f3563d7d4c8062b0d1575c6
Sayansky Nikolay <sarody@ya.ru>
2021-08-15 19:13:33 +0700

    header base

---
+++ src/components/header/header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './header.scss';
+import logoImg from './logo.svg';
+import userImg from './user.svg';
+import cartImg from './cart.svg';
+import enterImg from './enter.svg';
+
+function Header(){
+    return (
+        <div className='header'>
+            <div className='header__inner'>
+                <div className='header__section'>
+                    <div className='header__logo'>
+                        <img src={logoImg} alt='Pizza Sarody' width='200' height='46'/>
+                    </div>
+                </div>
+                <div className='header__section'>
+                    <div className='header__button'>
+                        <a href='#'>Пицца</a>
+                    </div>
+                    <div className='header__button'>
+                        <a href='#'>Акции</a>
+                    </div>
+                    <div className='header__button'>
+                        <a href='#'>О нас</a>
+                    </div>
+                    <div className='header__promo'>
+                        <input type='text'/>
+                        <button>
+                            <img src={enterImg} alt='enter' width='13' height='13'/>
+                        </button>
+                    </div>
+                    <div className='header__button'>
+                        <img src={userImg} alt='user' width='22' height='22'/>
+                        {/*<span>{user.name}</span>*/}
+                    </div>
+                    <div className='header__button'>
+                        <img src={cartImg} alt='cart' width='24' height='24'/>
+                        {/*<span>{user.name}</span>*/}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
