bd4bc46f0d5ae7fe5dbb83b92e2b8168bb19ae5e
Nikolay <sarody@ya.ru>
2023-03-24 15:08:55 +0800

    settings page layout

--- src/components/profile/settings/settings.tsx
+++ src/components/profile/settings/settings.tsx
@@ -2,13 +2,32 @@ import React from 'react';
 import {useAppDispatch} from '../../../hook';
 import ProfileNav from '../profileNav/profileNav';
 import {Pages} from '../../../models/models';
+import './settings.scss';
 
 const Settings: React.FC = () => {
     const dispatch = useAppDispatch();
 
     return (
-        <div>
+        <div className="settings">
             <ProfileNav page={Pages.Settings}/>
+            <div className="settings__container">
+                <h1>Личные данные</h1>
+                <form>
+                    <label>
+                        Логин
+                        <input type="text"/>
+                    </label>
+                    <label>
+                        Старый пароль
+                        <input type="text"/>
+                    </label>
+                    <label>
+                        Новый пароль
+                        <input type="text"/>
+                    </label>
+                    <input type="submit" value="Сохранить"/>
+                </form>
+            </div>
         </div>
     )
 }
